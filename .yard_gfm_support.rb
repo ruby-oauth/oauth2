ca63d419d7778691b8389ce9d23103493f13ce15
Peter H. Boling <peter.boling@gmail.com>
2025-11-06 21:02:34 -0700

    🐛 Fixing markdown => HTML conversion

--- .yard_gfm_support.rb
+++ .yard_gfm_support.rb
@@ -11,12 +11,24 @@ class KramdownGfmDocument < Kramdown::Document
   end
 end
 
+# Ensure YARD is loaded before modifying its constants
+require 'yard' unless defined?(YARD)
+
 # Insert the new provider as the highest priority option for Markdown.
 # See:
 # - https://github.com/lsegal/yard/issues/1157
 # - https://github.com/lsegal/yard/issues/1017
 # - https://github.com/lsegal/yard/blob/main/lib/yard/templates/helpers/markup_helper.rb
-YARD::Templates::Helpers::MarkupHelper::MARKUP_PROVIDERS[:markdown].insert(
-  0,
-  {const: "KramdownGfmDocument"},
-)
+require 'yard/templates/helpers/markup_helper'
+
+providers = YARD::Templates::Helpers::MarkupHelper::MARKUP_PROVIDERS[:markdown]
+providers.unshift({lib: :kramdown, const: :KramdownGfmDocument})
+
+# Normalize provider entries to what YARD expects (const must be a String)
+providers.each do |provider|
+  const = provider[:const]
+  provider[:const] = const.to_s if const.is_a?(Symbol)
+end
+
+# De-duplicate entries by [lib, const]
+providers.uniq! { |p| [p[:lib], p[:const].to_s] }
