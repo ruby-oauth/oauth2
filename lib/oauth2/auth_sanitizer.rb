89397f699bd6903c5ac3a9927d1743984a946dc5
autobolt <autobots@9thbit.net>
2026-05-20 14:33:10 -0600

    Constrain auth-sanitizer loader lookup

--- lib/oauth2/auth_sanitizer.rb
+++ lib/oauth2/auth_sanitizer.rb
@@ -2,12 +2,20 @@
 
 module OAuth2
   AUTH_SANITIZER = begin
-    auth_sanitizer_spec = Gem.loaded_specs["auth-sanitizer"] ||
-      Gem::Specification.find_by_name("auth-sanitizer")
+    auth_sanitizer_requirement = Gem::Requirement.new("~> 0.1", ">= 0.1.3")
+    auth_sanitizer_spec = Gem.loaded_specs["auth-sanitizer"]
+    unless auth_sanitizer_spec && auth_sanitizer_requirement.satisfied_by?(auth_sanitizer_spec.version)
+      auth_sanitizer_spec = Gem::Specification.find_by_name("auth-sanitizer", auth_sanitizer_requirement)
+    end
+
     auth_sanitizer_loader_path = File.join(
       auth_sanitizer_spec.full_gem_path,
       "lib/auth_sanitizer/loader.rb",
     )
+    unless File.file?(auth_sanitizer_loader_path)
+      raise LoadError, "oauth2 requires auth-sanitizer #{auth_sanitizer_requirement}; " \
+        "loader not found at #{auth_sanitizer_loader_path}"
+    end
 
     auth_sanitizer_loader_namespace = Module.new
     auth_sanitizer_loader_namespace.module_eval(
